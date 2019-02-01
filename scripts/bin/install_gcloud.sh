814f48b1d754bcbafbf280e9d0a99513e1e2945c
cornelk <github@codejuggle.dj>
2019-01-31 19:29:30 -0600

    update

--- scripts/bin/install_gcloud.sh
+++ scripts/bin/install_gcloud.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-# Create environment variable for correct distribution
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-
-# Add the Cloud SDK distribution URI as a package source
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-# Import the Google Cloud Platform public key
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-# Update the package list and install the Cloud SDK
-apt-get update && apt-get -y install google-cloud-sdk
+VERSION=232.0.0
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${VERSION}-linux-x86_64.tar.gz
+tar zxvf google-cloud-sdk-${VERSION}-linux-x86_64.tar.gz google-cloud-sdk
+./google-cloud-sdk/install.sh
