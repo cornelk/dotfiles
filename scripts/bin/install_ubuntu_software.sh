059f6ad94ca6d5aff448d1cd83b02038fbaf8e48
cornelk <github@codejuggle.dj>
2019-02-10 19:46:31 -0600

    update

--- scripts/bin/install_ubuntu_software.sh
+++ scripts/bin/install_ubuntu_software.sh
@@ -18,19 +18,19 @@ apt-get -y install cryptsetup-bin keepassxc oathtool
 apt-get -y install cmake ctags direnv git git-lfs jq meld nodejs npm pgadmin3 postgresql-client python-pip ruby
 
 # network
-apt-get -y install ansible autossh axel corkscrew curl fping iptraf mtr net-tools network-manager-openvpn-gnome nmap ntp openvpn syncthing whois
+apt-get -y install ansible autossh axel corkscrew curl fping iptraf mtr net-tools network-manager-openvpn-gnome nmap ntp openvpn syncthing ufw whois
 
 # emulation / virtualization
 apt-get -y install docker.io docker-compose dosbox virtualbox virtualbox-guest-additions-iso wine-development winetricks
 
 # gui
-apt-get -y install feh fonts-font-awesome fonts-inconsolata i3 i3lock-fancy lightdm lightdm-settings plymouth-theme-xubuntu-logo rofi slick-greeter snapd terminator unifont xbacklight
+apt-get -y install feh fonts-font-awesome fonts-inconsolata i3 i3lock-fancy lightdm lightdm-settings plymouth-theme-xubuntu-logo rofi slick-greeter terminator unifont xbacklight
 
 # multimedia
 apt-get -y install calibre gimp graphviz handbrake pandoc rawtherapee texlive-latex-base texlive-latex-recommended texlive-luatex vlc zbar-tools
 
 # system
-apt-get -y install apt-transport-https aptitude fish htop iotop mc neovim p7zip-full p7zip-rar powertop scrot sshpass stow tmux tree
+apt-get -y install acpi apt-transport-https aptitude fish htop iotop mc neovim p7zip-full p7zip-rar powertop scrot snapd sshpass stow tmux tree
 
 # development files for additional software to install
 apt-get -y install libcairo2-dev libxcb1-dev libxcb-composite0-dev libxcb-randr0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev python-xcbgen libasound2-dev libmpdclient-dev libiw-dev xcb-proto
