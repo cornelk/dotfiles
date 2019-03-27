458974b0a84767d45945afdad697e7f8c7a040fb
cornelk <github@codejuggle.dj>
2019-03-26 19:12:09 -0600

    update

--- scripts/bin/setup_zsh.sh
+++ scripts/bin/setup_zsh.sh
@@ -3,8 +3,9 @@
 # install on my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
+# change default shell of user to zsh
 chsh -s /usr/bin/zsh
 
