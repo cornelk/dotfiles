0dd3d89e99c00bacce5ea54cb08902a25f9f83a2
cornelk <github@codejuggle.dj>
2019-06-13 22:24:13 +0200

    update

--- scripts/bin/install_ubuntu_software.sh
+++ scripts/bin/install_ubuntu_software.sh
@@ -15,7 +15,7 @@ apt-get -y install alsa-tools alsa-tools-gui arandr blueman bluez-tools bumblebe
 apt-get -y install cryptsetup-bin keepassxc oathtool
 
 # development
-apt-get -y install clang clang-format cmake ctags direnv git git-lfs jq meld nodejs npm postgresql-client python-pip ruby shellcheck
+apt-get -y install clang clang-format cmake ctags direnv git git-lfs jq meld nodejs npm pkgconf postgresql-client python-pip ruby shellcheck
 
 # network
 apt-get -y install ansible autossh axel corkscrew curl fping iptraf mtr net-tools network-manager-openvpn-gnome nmap ntp openvpn syncthing traceroute ufw whois
@@ -30,7 +30,7 @@ apt-get -y install feh fonts-font-awesome fonts-inconsolata i3 i3lock-fancy ligh
 apt-get -y install calibre gimp graphviz handbrake pandoc rawtherapee texlive-latex-base texlive-latex-recommended texlive-luatex vlc zbar-tools
 
 # system
-apt-get -y install acpi apt-transport-https aptitude fish htop iotop mc msr-tools neovim p7zip-full p7zip-rar powertop scrot snapd sshpass stow sysbench tmux tree unrar zsh
+apt-get -y install acpi apt-transport-https aptitude dos2unix fish htop iotop mc msr-tools neovim p7zip-full p7zip-rar powertop scrot snapd sshpass stow sysbench tmux tree unrar zsh
 
 # development files for additional software to install
 apt-get -y install libcairo2-dev libxcb1-dev libxcb-composite0-dev libxcb-randr0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev python-xcbgen libasound2-dev libmpdclient-dev libiw-dev xcb-proto
