deb035e584a48f719a446eb77eb3774586116ae7
cornelk <github@codejuggle.dj>
2020-12-30 14:56:34 -0600

    update

--- scripts/bin/install_ubuntu_software.sh
+++ scripts/bin/install_ubuntu_software.sh
@@ -8,6 +8,8 @@ fi
 apt-get update
 apt-get -y upgrade
 
+# Ubuntu 20.10+
+
 # hardware
 apt-get -y install alsa-tools alsa-tools-gui arandr blueman bluez-tools bumblebee bumblebee-nvidia lm-sensors tlp
 
@@ -15,7 +17,7 @@ apt-get -y install alsa-tools alsa-tools-gui arandr blueman bluez-tools bumblebe
 apt-get -y install cryptsetup-bin keepassxc oathtool
 
 # development
-apt-get -y install android-tools-adb clang clang-format cmake ctags direnv git git-lfs jq meld nodejs npm pkgconf postgresql-client python-pip python3-pip python3-venv ruby shellcheck
+apt-get -y install adb clang clang-format cmake direnv git git-lfs jq meld nodejs npm pkgconf postgresql-client python3-pip python3-venv ruby shellcheck universal-ctags
 
 # java
 apt-get -y install default-jre
@@ -39,5 +41,5 @@ apt-get -y install acpi apt-transport-https aptitude dos2unix fish htop iotop mc
 apt-get -y install exfat-fuse exfat-utils
 
 # development files for additional software to install
-apt-get -y install libcairo2-dev libxcb1-dev libxcb-composite0-dev libxcb-randr0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev python-xcbgen libasound2-dev libmpdclient-dev libiw-dev xcb-proto libxcb-xkb-dev libpulse-dev python3-xcbgen libjsoncpp-dev
+apt-get -y install libcairo2-dev libxcb1-dev libxcb-composite0-dev libxcb-randr0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libasound2-dev libmpdclient-dev libiw-dev xcb-proto libxcb-xkb-dev libpulse-dev python3-xcbgen libjsoncpp-dev
 
