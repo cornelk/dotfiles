ce5830ccf3ae77416c917c1c2a0b9985981a02ef
cornelk <github@codejuggle.dj>
2021-06-13 11:31:29 -0600

    update

--- scripts/bin/layout_single.sh
+++ scripts/bin/layout_single.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-xrandr --output DP-1 --off --output eDP-1 --primary --mode 3000x2000 --pos 420x2160 --rotate normal
+xrandr --output eDP-1-1 --primary --mode 3000x2000 --pos 0x0 --rotate normal --scale 1x1
+
